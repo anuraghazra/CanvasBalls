1e87a26a027b4bdb0ea6e631de80b746fe085aa7
anuraghazra <hazru.anurag@gmail.com>
2019-11-21 13:27:16 +0530

    refactor: added Circle.js and refactored code

--- js/canvas.js
+++ js/canvas.js
@@ -1,68 +1,26 @@
-var canvas = document.querySelector('canvas');
-const height = window.innerHeight;
-const width = window.innerWidth;
-var c = canvas.getContext('2d');
-canvas.height = height;
-canvas.width = width;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const circles = [];
 
+for (let i = 0; i < 15; i++) {
+  const radius = 50;
+  const diameter = radius * 2
+  const x = Math.random() * (width - diameter) + radius;
+  const y = Math.random() * (height - diameter) + radius;
+  const dx = Math.random() - 0.5 * 3;
+  const dy = Math.random() - 0.5 * 5;
+  const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
 
-class Circle{
-    constructor(x, y, dx, dy, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
-        this.draw = () => {
-            c.beginPath();
-            c.fillStyle = color;
-            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            c.fill();
-            c.closePath();
-        }
-        this.update = () => {
-            this.draw();
-            if ((this.x + this.radius > width) || (this.x - this.radius < 0)) {
-                this.dx = -this.dx;
-            }
-            if ((this.y + this.radius > height) || (this.y - this.radius < 0)) {
-                this.dy = -this.dy;
-            }
-            this.x += this.dx;
-            this.y += this.dy;
-        }
-    }
+  circles.push(new Circle(x, y, dx, dy, radius, color))
 }
 
-
-
-let circles = [];
-for (let i = 0; i < 15; i++){
-    let radius = 50;
-    let diameter = radius * 2
-    let x = Math.random() * (width - diameter) + radius;
-    let y = Math.random() * (height - diameter) + radius;
-    let dx = Math.random() - 0.5 * 3;
-    let dy = Math.random() - 0.5 * 5;
-    let color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-    circles.push(new Circle(x, y, dx, dy, radius, color))
-}
 function animate() {
-    c.clearRect(0, 0, width, height);
-    circles.forEach(circle => {
-        circle.update();
-    })
-    requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, width, height);
+
+  circles.forEach(circle => circle.update(ctx))
+
+  requestAnimationFrame(animate);
 }
 animate();
-
-// canvas.addEventListener('mousemove', (e) => {
-//     if (e.which == 1) {
-//         c.beginPath();
-//         c.fillStyle = 'red';
-//         c.arc(e.x, e.y, 10, 0, Math.PI * 2, false);
-//         c.fill();
-//         c.closePath();
-//     }
-// })
-
