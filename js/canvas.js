8d4229f3106e30392ec43e63dca3519b60b0796f
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-09-01 18:38:14 +0330

    factored some parameters into config file

--- js/canvas.js
+++ js/canvas.js
@@ -1,26 +1,33 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const circles = [];
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
 
-for (let i = 0; i < 15; i++) {
-  const radius = 50;
-  const diameter = radius * 2
-  const x = Math.random() * (width - diameter) + radius;
-  const y = Math.random() * (height - diameter) + radius;
-  const dx = Math.random() - 0.5 * 3;
-  const dy = Math.random() - 0.5 * 5;
-  const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+const { width: canvasWidth, height: canvasHeight } = config.canvas
+const { radius: ballsRadius, count: ballsCount } = config.balls
 
-  circles.push(new Circle(x, y, dx, dy, radius, color))
+// setting width and height of the canvas
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
+const circles = []
+
+for (let i = 0; i < ballsCount; i++) {
+  const diameter = ballsRadius * 2
+  const x = Math.random() * (canvasWidth - diameter) + ballsRadius
+  const y = Math.random() * (canvasHeight - diameter) + ballsRadius
+  const dx = Math.random() - 0.5 * 3
+  const dy = Math.random() - 0.5 * 5
+  const color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+    Math.random() * 255
+  })`
+
+  circles.push(new Circle(x, y, dx, dy, ballsRadius, color))
 }
 
 function animate() {
-  ctx.clearRect(0, 0, width, height);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
   circles.forEach(circle => circle.update(ctx))
 
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animate)
 }
-animate();
+animate()
---
+++ js/config.js
@@ -0,0 +1,10 @@
+const config = {
+  canvas: {
+    height: innerHeight,
+    width: innerWidth
+  },
+  balls: {
+    count: 15,
+    radius: 50
+  }
+}
